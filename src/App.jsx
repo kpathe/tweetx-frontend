971b4d92e251e6015e5c62006d155ae61e51dedb
Kaushlendra Pathe <kpathe972@gmail.com>
2026-03-18 10:29:11 +0530

    add the userlogin in check whenever the app runs for the first time

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
-import "./App.css";
-import authService from "../src/services/auth.service";
+import { useEffect, useState } from "react";
+import userService from "../src/services/user.service";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice"; // Import the actions!
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loader, setLoader] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // We call the service and handle the Promise
+    userService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoader(false)); 
+  }, []); // 
+
   
+  if (loader) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
 
   return (
-    <>
-      <h1 className="text-center my-20 w-full text-violet-500 text-5xl">
-        tweetX
-      </h1>
-    </>
+    <div className="min-h-screen bg-white dark:bg-slate-900">
+      <div className="w-full block">
+        <h1>TweetX</h1>
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
