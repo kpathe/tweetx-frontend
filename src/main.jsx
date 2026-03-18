4421cf87835ec013bafb0c5535e706f7a531901d
Kaushlendra Pathe <kpathe972@gmail.com>
2026-03-18 11:52:16 +0530

    add routing for pages in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,46 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "../src/store/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Protected from "./components/AuthLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Protected authentication={true}>
+            <Home />
+          </Protected>
+        ),
+      },
+      {
+        path: "/login",
+        element: (
+          <Protected authentication={false}>
+            <Login />
+          </Protected>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Protected authentication={false}>
+            <Signup />
+          </Protected>
+        ),
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 );
