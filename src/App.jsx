60d12c1f2affdc27803dc936c851983393aa02ff
Kaushlendra Pathe <kpathe972@gmail.com>
2026-03-18 09:56:21 +0530

    add the Protected component

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,13 @@ import "./App.css";
 import authService from "../src/services/auth.service";
 
 function App() {
-  const [fullName, setfullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleFullName = (e) => {
-    setfullName(e.target.value);
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSignup = (e) => {
-    e.preventDefault();
-    console.log(fullName,email,password);
-    
-    authService.createAccount({ fullName, email, password });
-  };
+  
 
   return (
     <>
       <h1 className="text-center my-20 w-full text-violet-500 text-5xl">
         tweetX
       </h1>
-
-      <div>
-        <form onSubmit={handleSignup}>
-          <label htmlFor="fullName">Full Name :</label>
-          <input onChange={handleFullName} type="text" />
-          <label htmlFor="email">Email :</label>
-          <input onChange={handleEmail} type="email" />
-          <label htmlFor="password">Password :</label>
-          <input onChange={handlePassword} type="password" />
-
-          <button type="submit">Signup</button>
-        </form>
-      </div>
     </>
   );
 }
---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+
+function Protected({ children, authentication = true }) {
+  const [loader, setLoader] = useState(true);
+  const navigate = useNavigate();
+  
+  
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+   
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } 
+   
+    else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <>Loading...</> : <>{children}</>;
+}
+
+export default Protected;
