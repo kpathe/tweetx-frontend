971de8b83397505ef75dcab910687765aa5c7fa2
Kaushlendra Pathe <kpathe972@gmail.com>
2026-03-20 22:35:14 +0530

    add tweetReducer in the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import themeReducer from "./themeSlice";
+import tweetReducer from "./tweetSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer, theme: themeReducer },
+  reducer: { auth: authReducer, theme: themeReducer, tweet: tweetReducer },
 });
 
 export default store;
