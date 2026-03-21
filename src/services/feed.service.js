2b9ad50423b6f72059411fa41bb59bf36eaee964
Kaushlendra Pathe <kpathe972@gmail.com>
2026-03-21 09:32:13 +0530

    Add getHomeFeed service

--- src/services/feed.service.js
+++ src/services/feed.service.js
@@ -3,6 +3,16 @@ import apiClient from "../api/axios";
 class FeedService {
   async getHomeFeed() {
     try {
+      const response = await apiClient.get("/home-feed");
+      return response.data;
+    } catch (error) {
+      console.error("FeedService :: getHomeFeed :: error", error);
+      throw error;
+    }
+  }
+
+  async getFeed() {
+    try {
       const response = await apiClient.get("/feed");
       return response.data;
     } catch (error) {
@@ -10,6 +20,8 @@ class FeedService {
       throw error;
     }
   }
+
+  
 }
 
 const feedService = new FeedService();
