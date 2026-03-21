c7ae66bff856f3baf02157de98134b7e071f8928
Kaushlendra Pathe <kpathe972@gmail.com>
2026-03-21 22:53:04 +0530

    remove Link

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { login as authLogin } from "../store/authSlice";
 import { Button, Input } from "./index";
 import { useDispatch } from "react-redux";
