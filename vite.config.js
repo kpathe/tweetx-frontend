cbbbebbd82bcab46fe37364aff81a40d1921ad11
Kaushlendra Pathe <kpathe972@gmail.com>
2026-04-04 20:40:49 +0530

    add server config

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: "/",
+  server: {
+    host: true, // Allow external access
+    port: 5173, 
+    strictPort: true,
+  },
 });
